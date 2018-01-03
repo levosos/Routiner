288a0fae57d771bf8bc530018d2467537c70eb2d
Raz Haleva <razhaleva@gmail.com>
2018-01-03 15:26:18 +0200

    Basic integration with Covalent Teradata

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material';
+import {
+         MatButtonModule,
+         MatCardModule,
+         MatListModule,
+         MatIconModule,
+       } from '@angular/material';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { CovalentLayoutModule } from '@covalent/core';
 
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
@@ -17,8 +23,12 @@ import { AuthService } from './services/auth.service';
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
     AngularFireModule.initializeApp(environment.firebase, 'routiner'),
     AngularFireAuthModule,
+    CovalentLayoutModule,
   ],
   providers: [
     AuthService,
